61b28b813f28ffd4ea4a8de505be2838148fb59b
Aleksandra <aleksandrapozarickaa@gmail.com>
2026-07-20 18:04:13 +0000

    Create pseudocode

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 console.log('Hello World');
+
+// Create function getComputerChoice
+//     Generate randomNumber between 0 and 1 using Math.random
+//     If randomNumber < 0.33 then return rock
+//     Else if randomNumber < 0.66 then return paper
+//     Else randomNumber < 1 then return scissors
+
+// Create function getHumanChoice
+//     Use prompt to receive human choice 
+
+// Create and initialize with the value of 0 varible humanScore
+// Create and initialize with the value of 0 varible computerScore
+
+// Create function playRound with 2 parameters humanScore and computerScore
+//     make parameter humanScore case-insensitive
+//     If human win then display corresponding message and increment variable humanScore
+//     Else if computer win then display corresponding message and increment variable computerScore
+
+// Create function playGame
+//     call function playRound 5 times
